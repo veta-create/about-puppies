57501aa5fdae79eedb73256afeafcd696dab2a39
@veta-create <vita.eksuzyan@bk.ru>
2022-09-06 11:38:04 +0300

    edits

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
-import RelationshipBlock from './components/RelathionshipBlock/RelationshipBlock';
+import {RelationshipBlock} from './components/RelathionshipBlock/RelationshipBlock';
 
 function App() {
   return (
--- src/components/RelathionshipBlock/RelationshipBlock.jsx
+++ src/components/RelathionshipBlock/RelationshipBlock.jsx
@@ -1,19 +1,14 @@
 import styles from './Relationship.module.css'
-import background from '../../assets/images/background.png'
 import arrow from '../../assets/images/arrow.png'
 
-function RelationshipBlock () {
+export function RelationshipBlock () {
     return (
         <div className={styles.main}>
-            <img src={background} alt='yellow background' />
             <div className={styles.description}>
                 <h1>Taking care<br /> for you Smart Dog !</h1>
                 <p>Human-canine bonding is the relationship<br /> between dogs and humans</p>
-                <button><div>Explore more</div>
-                <img className={styles.arrow} src={arrow} alt='arrow' /></button>
+                <button>Explore more</button>
             </div>
         </div>
     )
 }
-
-export default RelationshipBlock
