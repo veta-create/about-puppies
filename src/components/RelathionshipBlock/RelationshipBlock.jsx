7514928ccc5499f1f8ce79b5f3eb8eb302678b64
@veta-create <vita.eksuzyan@bk.ru>
2022-09-06 15:43:58 +0300

    create-services-block

--- src/components/RelathionshipBlock/RelationshipBlock.jsx
+++ src/components/RelathionshipBlock/RelationshipBlock.jsx
@@ -9,6 +9,19 @@ export function RelationshipBlock () {
                 <p>Human-canine bonding is the relationship<br /> between dogs and humans</p>
                 <button>Explore more</button>
             </div>
+
+            <div className={styles.services}>
+                <div className={styles.header}><h1>Services Category</h1></div>
+                <div className={styles.navContainer}>
+                    <nav>
+                        <div className={styles.grooming}><a href="#">Grooming</a></div>
+                        <div className={styles.healthcare}><a href="#">Healthcare</a></div>
+                        <div className={styles.daycare}><a href="#">Daycare</a></div>
+                        <div className={styles.training}><a href="#">Training</a></div>
+                        <div className={styles.hyginic}><a href="#" >Hyginic care</a></div>
+                    </nav>
+                </div>
+            </div>
         </div>
     )
 }
