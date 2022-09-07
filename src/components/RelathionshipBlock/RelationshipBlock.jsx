6dd4c29c0928452c38f9eeb1a7280ec5b6ce75db
@veta-create <vita.eksuzyan@bk.ru>
2022-09-07 10:20:32 +0300

    icons svg, dog img

--- src/components/RelathionshipBlock/RelationshipBlock.jsx
+++ src/components/RelathionshipBlock/RelationshipBlock.jsx
@@ -1,14 +1,20 @@
 import styles from './Relationship.module.css'
-import arrow from '../../assets/images/arrow.png'
+import dog from '../../assets/images/dog1.png'
 
 export function RelationshipBlock () {
     return (
         <div className={styles.main}>
+          <div className={styles.relationship}>
             <div className={styles.description}>
                 <h1>Taking care<br /> for you Smart Dog !</h1>
                 <p>Human-canine bonding is the relationship<br /> between dogs and humans</p>
                 <button>Explore more</button>
             </div>
+
+            <div>
+                <img src={dog} />
+            </div>
+           </div>
 
             <div className={styles.services}>
                 <div className={styles.header}><h1>Services Category</h1></div>
