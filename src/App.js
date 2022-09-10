48c39eb0f44c25eadb4e7d99c2c20a26c084a6a6
@veta-create <vita.eksuzyan@bk.ru>
2022-09-10 11:42:43 +0300

    finished block about dogs do speak

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
+import './App.css'
 import Header from './components/Header/Header';
-import {RelationshipBlock} from './components/RelathionshipBlock/RelationshipBlock';
+import { RelationshipBlock } from './components/RelathionshipBlock/RelationshipBlock';
+import { DogsDoSpeakBlock } from './components/DogsDoSpeakBlock/DogsDoSpeakBlock';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <RelationshipBlock />
+      <DogsDoSpeakBlock />
       <div className='mainContent'>
 
       </div>
