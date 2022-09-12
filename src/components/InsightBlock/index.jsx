f82bb6014d1239ff4af41ea4ca61beccdfd1b06f
@veta-create <vita.eksuzyan@bk.ru>
2022-09-12 12:26:16 +0300

    finished insight-block

---
+++ src/components/InsightBlock/index.jsx
@@ -0,0 +1,22 @@
+import styles from "./style.module.css";
+
+export function InsightBlock() {
+  return (
+    <section className={styles.main}>
+      <div className={styles.description}>
+        <h2>
+          No one appreciates the very
+          <br /> special genius of your
+          <br /> conversation as the dog does.
+        </h2>
+        <p>
+          Sweet roll ice cream powder candy canes ice cream
+          <br /> donut pudding biscuit ice cream.biscuit caramels topb
+        </p>
+        <button>
+          <span>Explore more</span>
+        </button>
+      </div>
+    </section>
+  );
+}
