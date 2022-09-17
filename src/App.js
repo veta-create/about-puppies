4395d61ee5ea1b13ce9e4f08ea66681abd852caa
@veta-create <vita.eksuzyan@bk.ru>
2022-09-17 11:34:37 +0300

    import footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css'
-import Header from './components/Header/Header';
-import {RelationshipBlock} from './components/RelathionshipBlock/index';
-import {DogsDoSpeakBlock} from './components/DogsDoSpeakBlock/index'
-import { DogFoodBlock } from './components/DogFoodBlock/index';
-import {InsightBlock} from './components/InsightBlock/index'
-import { BlockAboutCustomers } from './components/BlockAboutCustomers';
+import "./App.css";
+import Header from "./components/Header/Header";
+import { RelationshipBlock } from "./components/RelathionshipBlock/index";
+import { DogsDoSpeakBlock } from "./components/DogsDoSpeakBlock/index";
+import { DogFoodBlock } from "./components/DogFoodBlock/index";
+import { InsightBlock } from "./components/InsightBlock/index";
+import { BlockAboutCustomers } from "./components/BlockAboutCustomers";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <DogFoodBlock />
       <InsightBlock />
       <BlockAboutCustomers />
+      <Footer />
     </div>
   );
 }
