e3c3c7ba6a9259bc2ee1838a8abe23a95e25fcb3
@veta-create <vita.eksuzyan@bk.ru>
2022-09-19 12:09:38 +0300

    adaptive header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,27 @@
-import styles from './Header.module.css'
-import logo from '../../assets/images/logo.png'
+import styles from "./Header.module.css";
+import logo from "../../assets/images/logo.png";
 
 function Header() {
-    return (
-        <section className={styles.header}>
-            <div className={styles.logo}>
-                <div><img alt='logo' src={logo}></img></div>
-            </div>
-            <div className={styles.navigation}>
-                <nav>
-                    <a href="#">Our services</a>
-                    <a href="#">About us</a>
-                    <a href="#">Products</a>
-                    <a href="#">Pet care</a>
-                    <a href="#">Contact</a>
-                </nav>
-            </div>
-            <div className={styles.auth}>
-                <button className={styles.sign}>Sign in</button>
-                <button className={styles.register}>Register</button>
-            </div>
-        </section>
-    )
+  return (
+    <section className={styles.header}>
+      <div className={styles.logo}>
+        <img alt="logo" src={logo}></img>
+      </div>
+      <div className={styles.navigation}>
+        <nav>
+          <a href="#">Our services</a>
+          <a href="#">About us</a>
+          <a href="#">Products</a>
+          <a href="#">Pet care</a>
+          <a href="#">Contact</a>
+        </nav>
+      </div>
+      <div className={styles.auth}>
+        <button className={styles.sign}>Sign in</button>
+        <button className={styles.register}>Register</button>
+      </div>
+    </section>
+  );
 }
 
-export default Header
+export default Header;
